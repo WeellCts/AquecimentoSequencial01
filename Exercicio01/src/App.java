8b4b288171b5d4941d38f14f6f69db056c24acc2
WeellCts <wellingtonmccosta67@gmail.com>
2026-09-02 22:22:22 +0000

    Exercicio01

--- Exercicio01/src/App.java
+++ Exercicio01/src/App.java
@@ -4,31 +4,20 @@ public class App {
     public static void main(String[] args)  {
         Scanner leitor = new Scanner(System.in);
         
-        // ENTRADA
-        System.out.print("Nome do produto: Olá, wellington");
-        String nome = leitor.next();
-        System.out.print("Preço: ");
-        double preco = leitor.nextDouble();
-        System.out.print("Quantidade: ");
-        int qtd = leitor.nextInt();
+     // ENTRADA
+        System.out.print("Digite seu nome: ");
+        String nome = leitor.nextLine();
 
-        //PROCESSAMENTO
-        double total = preco * qtd;
-        double desconto = 0.0;
+        System.out.print("Digite sua idade: ");
+        int idade = leitor.nextInt();
 
-        //DECISÃO
-        if (total >= 100.0) {
-            desconto = total * 0.10;
-        }
+        // PROCESSAMENTO
+        String mensagem = "Olá, " + nome + ", você tem " + idade + " anos!";
 
-        //SAÍDA
-        double valorFinal = total - desconto;
-        System.out.println("--- Resumo ---");
-        System.out.println("Subtotal: R$ " + total);
-        System.out.println("Desconto: R$ " + desconto);
-        System.out.println("Total a Pagar: R$ " + valorFinal);
+        // SAÍDA
+        System.out.println(mensagem);
 
-     leitor.close();
-
+        leitor.close();
+  
     }
 }
